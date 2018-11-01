00c63e9078466c74c3f26cbb5fbc1eb53178f8ba
JiSeong Nam <njs8590@gmail.com>
2018-11-01 20:35:55 +0900

    Initial commit_jiseong

---
+++ app/src/main/java/com/example/jiseongnam/ebsproj1/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.jiseongnam.ebsproj1;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase database;
+
+    Button mp3btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mp3btn = (Button)findViewById(R.id.bt_mp3load);
+        mp3btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //database = FirebaseDatabase.getInstance();
+                Toast.makeText(getApplicationContext(),"mp3 loading..", Toast.LENGTH_SHORT).show();
+
+                MediaPlayer mediaPlayer = new MediaPlayer();
+                try
+                {
+                    mediaPlayer.setDataSource("https://firebasestorage.googleapis.com/v0/b/ebsproj1.appspot.com/o/07%20%EC%9E%85%EC%9D%B4%20%ED%8A%B8%EC%9D%B4%EB%8A%94%20%EC%98%81%EC%96%B4_07.mp3?alt=media&token=64adf3a6-c171-4535-bfbd-e588fe8eafa2");
+                    mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            mp.start();
+                        }
+                    });
+                    mediaPlayer.prepare();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+}
