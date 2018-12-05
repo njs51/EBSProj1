bb58feea3b4c6a5874495cfad11efb733120921a
JiSeong Nam <njs8590@gmail.com>
2018-12-05 18:01:14 +0900

    20181205_jiseong

--- app/src/main/java/com/example/jiseongnam/ebsproj1/splashActivity.java
+++ app/src/main/java/com/example/jiseongnam/ebsproj1/splashActivity.java
@@ -2,16 +2,26 @@ package com.example.jiseongnam.ebsproj1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class splashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        startLoading();
         Intent intent = new Intent(this,HomeActivity.class);
         intent.putExtra("state","launch");
         startActivity(intent);
         finish();
+    }
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 5000);
     }
 }
